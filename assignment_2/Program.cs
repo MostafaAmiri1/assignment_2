a792be398707d6541011ad672599f68eb634f9d4
MostafaAmiri1 <amiri.mostafa45@gmail.com>
2024-01-22 22:24:09 +0100

    commit nr. 2

--- assignment_2/Program.cs
+++ assignment_2/Program.cs
@@ -6,5 +6,16 @@ class Program
     {
         Console.WriteLine("Hello, World!");
         Console.WriteLine("Hello, my name is Mostafa");
+        
+        CountTo(5);
+    }
+ 
+    static void CountTo(int num)
+    {
+        for (int i = 1; i <= num; i++)
+        {
+            Console.WriteLine(i);
+        }
     }
 }
+
